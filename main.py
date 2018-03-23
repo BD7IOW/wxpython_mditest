dc391f356c5a0da3c361e38412f129c892e4e154
JZY <a693930096@gmail.com>
2018-03-23 20:59:36 +0800

    base function

--- main.py
+++ main.py
@@ -9,39 +9,71 @@ import configparser
 from serconf import sercom
 from eclconf import exlconf
 # print instrument.read_register(289, 1)  # Remember to use print() for Python3
+
 import time
 import threading
 from queue import Queue
 from wx.lib.pubsub import pub
 class serThread(threading.Thread):
-    def __init__(self,eve,quent,conf,com,com1,bard,func,fmt,regs,rege,devs,deve,bsers,bsere):
+    def __init__(self,eve,quent,quent1,conf):
         super( serThread, self).__init__()
         self.data=quent
+        self.data1 = quent1
         self.conf=conf
-        self.com=com
-        self.bard=bard
-        if func is "03":
-           self.func=cst.READ_HOLDING_REGISTERS
-        elif func is "04":
-            self.func=cst.READ_INPUT_REGISTERS
-        self.fmt=fmt
-        self.regs=regs
-        self.rege=rege
-        self.devs=devs
-        self.deve=deve
-        self.bsers=bsers
-        self.bsere=bsere
+        self.com=""
+        self.com1=""
+        self.fun=0
+        self.bard=9600
+
+        self.fmt=""
+        self.regs=1
+        self.rege=1
+        self.devs=1
+        self.deve=1
+        self.bsers=1
+        self.bsere=1
         self.err=False
+        self.timout1=False
+        self.timout2 = False
         self.cnt = 0
         self.cnt1 = 0
         self.eve=eve
+        self.dat_init()
+    def dat_init(self):
+        regas = self.conf.get( "ser", "regadd" )
+        reg = regas.split( '#' )
+        regnum = [int( x ) for x in reg]
+        fmt = ">"
+        if self.conf.get( "ser", "dfmt" ) == "float":
+            for i in range( 5 ):
+                fmt = fmt + "f"
+        elif self.conf.get( "ser", "dfmt" ) == "int16":
+            for i in range( max( regnum ) ):
+                fmt = fmt + "h"
+        elif self.conf.get( "ser", "dfmt" ) == "u16":
+            for i in range( max( regnum ) ):
+                fmt = fmt + "H"
+        self.fmt = fmt
         # self.logger = modbus_tk.utils.create_logger( "console" )
+        self.com = self.conf.get( "ser", "sera" )
+        self.com1 = self.conf.get( "ser", "serb" )
+        self.bard = self.conf.getint( "ser", "bord" )
+        if self.conf.get( "ser", "func" ) == "03":
+           self.fun=cst.READ_HOLDING_REGISTERS
+        elif self.conf.get( "ser", "func" ) == "04":
+            self.fun=cst.READ_INPUT_REGISTERS
+        self.regs = min( regnum )
+        self.rege =max( regnum )
+        self.devs = self.conf.getint( "ser", "Astart" )
+        self.deve = self.conf.getint( "ser", "Aend" )
+        self.bsers = self.conf.getint("ser", "Bstart" )
+        self.bsere = self.conf.getint( "ser", "Bend" )
         try:
             # Connect to the slave
             self.master = modbus_rtu.RtuMaster(
-                serial.Serial( port=com, baudrate=bard, bytesize=8, parity='N', stopbits=1, xonxoff=0 )
+                serial.Serial( port=self.com, baudrate=self.bard, bytesize=8, parity='N', stopbits=1, xonxoff=0 )
             )
-            self.master.set_timeout( 1.0 )
+            self.master.set_timeout( 0.2 )
             self.master.set_verbose( True )
             # self.logger.info( "connected" )
 
@@ -49,7 +81,7 @@ class serThread(threading.Thread):
             self.dlg = wx.MessageDialog( None, u"Modbus串口打开失败！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
             if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
                 self.dlg.Destroy()
-            self.err =True
+
             # if dlg.ShowModal() == wx.ID_YES:
             #    dlg.Destroy()
             # self.logg "%s- Code=%d", exc, exc.get_exception_code() )
@@ -58,7 +90,7 @@ class serThread(threading.Thread):
             try:
                 # Connect to the slave
                 self.master1 = modbus_rtu.RtuMaster(
-                    serial.Serial( port=com1, baudrate=bard, bytesize=8, parity='N', stopbits=1, xonxoff=0 )
+                    serial.Serial( port=self.com1, baudrate=self.bard, bytesize=8, parity='N', stopbits=1, xonxoff=0 )
                 )
                 self.master1.set_timeout( 1.0 )
                 self.master1.set_verbose( True )
@@ -68,34 +100,53 @@ class serThread(threading.Thread):
                 self.dlg = wx.MessageDialog( None, u"Modbus串口打开失败！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
                 if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
                     self.dlg.Destroy()
-                self.err = True
+                # self.err = True
 
         # self.start()
     def run(self):
       while True:
         if not self.eve.is_set():
             self.eve.wait()
+        try:
+           if self.data1.get(block=False)=="update":
+               self.master.__del__()
+               if self.serBiScheck:
+                  self.master1.__del__()
+               self.dat_init()
+        except:pass
         if self.err is False:#self.devs+self.cnt
            try:
-             dat = self.master.execute( self.devs+self.cnt, self.func, self.regs, self.rege, data_format=self.fmt )
-           except:
-               self.dlg = wx.MessageDialog( None, u"Modbus读数据超时！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
-               if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
-                   self.dlg.Destroy()
-           num=(self.cnt+self.devs,)
-           self.data.put(num+dat)
+             dat = self.master.execute( self.devs+self.cnt, self.fun, self.regs, self.rege, data_format=self.fmt )
+           except: self.timout1=True
+               # self.dlg = wx.MessageDialog( None, u"Modbus读数据超时！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
+               # if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
+               #     self.dlg.Destroy()
+           if  self.timout1:
+               num=(self.cnt+self.devs,)
+               self.data.put(num+())
+               self.timout1 = False
+           else:
+               num = (self.cnt + self.devs,)
+               self.data.put( num + dat )
+           # dat=
            # print(num+dat)
-           # wx.CallAfter( pub.sendMessage,'update', msg=dat)
+           # wx.CallAfter(pub.sendMessage,'update', msg=dat)
            if self.serBiScheck:
                try:
-                   dat1 = self.master1.execute( self.bsers + self.cnt1, self.func, self.regs, self.rege,
+                   dat1 = self.master1.execute( self.bsers + self.cnt1, self.fun, self.regs, self.rege,
                                               data_format=self.fmt )
-               except:
-                   self.dlg = wx.MessageDialog( None, u"Modbus读数据超时！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
-                   if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
-                       self.dlg.Destroy()
-               num1 = (self.cnt1 + self.bsers+self.deve,)
-               self.data.put( num1 + dat1 )
+               except:self.timout2 = True
+                   # self.dlg = wx.MessageDialog( None, u"Modbus读数据超时！", u"Warning", wx.YES_NO | wx.ICON_WARNING )
+                   # if self.dlg.ShowModal() == wx.ID_YES or self.dlg.ShowModal() == wx.ID_NO:
+                   #     self.dlg.Destroy()
+               if self.timout2:
+                  num1 = (self.cnt1 + self.bsers+self.deve,)
+                  self.data.put( num1 + () )
+                  self.timout2 = False
+               else:
+                   num1 = (self.cnt1 + self.bsers + self.deve,)
+                   self.data.put( num1 + dat1 )
+               # dat1=[]
                self.cnt1 += 1
                if self.cnt1 + self.bsers > self.bsere:
                    self.cnt1 = 0
@@ -128,6 +179,7 @@ class ser1Thread(threading.Thread):
       while True:
         if not self.eve.is_set():
             self.eve.wait()
+        #     break
         val = self.data.get()
         self.data_temp.append(list(val))
         self.num+=1
@@ -143,10 +195,13 @@ class MainWindow(wx.Frame):
     def __init__(self, parent,title):
         wx.Frame.__init__( self, parent, id=wx.ID_ANY, title=title, pos=wx.DefaultPosition,
                            size=wx.Size( 652, 490 ), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL )
-
+        self.modbus_cnt = False#用于线程更新配置
         self.modbus_run = False
         self.swit = 0
         self.localtime=""
+        self.redad=[]
+        self.dis_fmt=[]
+        self.dis_fmt_name=[]
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
         bSizer5 = wx.BoxSizer( wx.VERTICAL )
@@ -157,7 +212,7 @@ class MainWindow(wx.Frame):
 
         bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.m_staticText12 = wx.StaticText( self.m_panel6, wx.ID_ANY, u"数据集：", wx.DefaultPosition, wx.DefaultSize,
+        self.m_staticText12 = wx.StaticText( self.m_panel6, wx.ID_ANY, u"数据采集", wx.DefaultPosition, wx.DefaultSize,
                                              wx.ALIGN_CENTRE | wx.ST_NO_AUTORESIZE )
         self.m_staticText12.Wrap( -1 )
         self.m_staticText12.SetFont(
@@ -186,34 +241,38 @@ class MainWindow(wx.Frame):
         self.m_grid2 = wx.grid.Grid( self.m_panel7, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
 
         # Grid
-        self.m_grid2.CreateGrid( 20, 5 )
-        self.m_grid2.EnableEditing( True )
-        self.m_grid2.EnableGridLines( True )
-        self.m_grid2.EnableDragGridSize( False )
-        self.m_grid2.SetMargins( 0, 0 )
+        self.m_grid2.CreateGrid( 256, 20 )
+        # self.m_grid2.EnableEditing( True )
+        # self.m_grid2.EnableGridLines( True )
+        # self.m_grid2.EnableDragGridSize( False )
+        # self.m_grid2.SetMargins( 0, 0 )
 
+        self.m_grid2.SetDefaultColSize( 60 )
+        self.m_grid2.SetDefaultRowSize( 20 )
+        self.m_grid2.SetDefaultCellOverflow(False)
         # Columns
-        self.m_grid2.SetColSize( 0, 58 )
-        self.m_grid2.SetColSize( 1, 30 )
-        self.m_grid2.SetColSize( 2, 30 )
-        self.m_grid2.SetColSize( 3, 31 )
-        self.m_grid2.SetColSize( 4, 29 )
-        self.m_grid2.AutoSizeColumns()
-        self.m_grid2.EnableDragColMove( False )
-        self.m_grid2.EnableDragColSize( True )
-        self.m_grid2.SetColLabelSize( 30 )
+
+        # self.m_grid2.SetColSize( 0, 58 )
+        # self.m_grid2.SetColSize( 1, 30 )
+        # self.m_grid2.SetColSize( 2, 30 )
+        # self.m_grid2.SetColSize( 3, 31 )
+        # self.m_grid2.SetColSize( 4, 29 )
+        # self.m_grid2.AutoSizeColumns()
+        # self.m_grid2.EnableDragColMove( False )
+        # self.m_grid2.EnableDragColSize( True )
+        # self.m_grid2.SetColLabelSize( 30 )
         self.m_grid2.SetColLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
 
         # Rows
-        self.m_grid2.AutoSizeRows()
-        self.m_grid2.EnableDragRowSize( True )
-        self.m_grid2.SetRowLabelSize( 30 )
+        # self.m_grid2.AutoSizeRows()
+        # self.m_grid2.EnableDragRowSize( True )
+        # self.m_grid2.SetRowLabelSize( 30 )
         self.m_grid2.SetRowLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
 
         # Label Appearance
 
         # Cell Defaults
-        self.m_grid2.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
+        self.m_grid2.SetDefaultCellAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
         bSizer7.Add( self.m_grid2, 0, wx.EXPAND, 5 )
 
         self.m_panel7.SetSizer( bSizer7 )
@@ -266,6 +325,10 @@ class MainWindow(wx.Frame):
         self.Bind( wx.EVT_TIMER, self.OnTimer, self.timer )  # 绑定一个定时器事件
         self.timer.Start( 500 )  # 设定时间间隔为1000毫秒,并启动定时器
         self.queue = Queue()  # 队列实例化
+        self.queue1 = Queue()  # 队列实例化
+        self.drawcollable()
+        self.update_redad()
+        self.dis_fmt_update()
     def OnTimer(self, evt):  # 显示时间事件处理函数
         self.localtime = time.strftime( "%Y-%m-%d %H:%M:%S", time.localtime() )
         self.m_statusBar2.SetStatusText( self.localtime, 1 )
@@ -299,6 +362,8 @@ class MainWindow(wx.Frame):
 
             self.conf.write(open("conf.ini","w"))
             dig.Destroy()
+            self.drawcollable()
+            self.update_redad()
 
 
     def exlconf(self,event):
@@ -325,28 +390,106 @@ class MainWindow(wx.Frame):
             self.conf.write( open( "conf.ini", "w" ) )
 
             di.Destroy()
+        self.dis_fmt_update()
+    def drawcollable(self):
+        r=2
+        colt1 = self.conf.get( "ser", "name" )
+        colt = colt1.split( '#' )
+        self.m_grid2.SetColLabelValue( 0, u"设备名称" )
+        self.m_grid2.SetColLabelValue( 1, u"设备ID" )
+        for i in colt:
+          self.m_grid2.SetColLabelValue(r,i)
+          r+=1
+    def update_redad(self):
+        #
+        ad=self.conf.get("ser","regadd" )
+        self.redad=ad.split('#')
+        self.redad = [int(x) for x in self.redad]
+
+    def data_filter(self,s):
+        out=[0, ]
+        # t=1
+        # print( self.redad )
+        out[0]=s[0]
+
+        try:
+           for i in self.redad:
+               out.append(s[i])
+               # print(i)
+               # t+=1
+        except:pass
+        return out
+    def update_oneLine(self,d):
+        # self.m_grid2.SetCellValue( i,0, self.dev[i][0] )
+        t=1
+        for i in d:
+            self.m_grid2.SetCellValue(d[0]-1 , t, str(i) )
+            t+=1
+    def data_extraction(self,c,s):#根据分类进行数据抽取
+        m=[]
+        for i in s:
+            if i[0] in c:
+                m.append(i)
+        return m
+
+    def fmt_dis_update(self,d,r):
+        t = 1
+        for i in d:
+            self.m_grid2.SetCellValue( 0+r, t, str( i ) )
+            t += 1
     def ser_data_update(self,msg=None):
-          print(msg)
+        print(msg)
+        # devnum=0
 
-
+        # if self.conf.getboolean("ser","B_ischeck"):
+        #     devnum= self.conf.getint("ser","Aend")-self.conf.getint("ser","Astart")+2+self.conf.getint("ser","Bend")-self.conf.getint("ser","Bstart")
+        # else: devnum= self.conf.getint("ser","Aend")-self.conf.getint("ser","Astart")+1
+        if self.conf.getboolean("excel","disp_set") is False:
+            self.m_staticText12.SetLabelText( "数据采集" )
+            for i in msg:
+                 # print(self.data_filter(i))
+                 self.update_oneLine(self.data_filter(i))
+            # self.m_grid2.SetCellValue( 1, 0, "222")
+            # self.m_grid2.CreateGrid(devnum, 10 )
+            # pass
+        else:
+            dis_ord=self.dis_fmt[self.swit]#已分类的地址
+            indata = self.data_extraction(dis_ord, msg )
+            print(indata)
+            r=0
+            for i in indata:
+                self.fmt_dis_update( self.data_filter(i),r )
+                r+=1
 
 
     def run(self, event):
         event.Skip()
+
+
+        # print( max(regnum))
         if self.modbus_run is not True:
             self.modbus_run=True
             self.m_button8.SetLabel(u"停止")
             self.m_statusBar2.SetStatusText( u"软件状态：" + "运行中", 0 )
             #self,quent,conf,com,com1,bard,func,fmt,regs,rege,devs,deve,bsers,bsere
-            self.serial1Thread = serThread(self.ser_clc_event,self.queue,self.conf,"COM2","COM4",9600,"04",">fffff",1,10,1,1,1,1)
 
-            self.clcThread = ser1Thread(self.ser_event,self.queue,self.conf)
 
-            self.serial1Thread.start()
-            self.clcThread.start()
+            if self.modbus_cnt is not True:
+                self.serial1Thread = serThread(self.ser_clc_event,self.queue,self.queue1,self.conf)#self.conf.getint( "ser", "bord" )self.conf.get("ser","bord")self.conf.get("ser","serb")
+
+                self.clcThread = ser1Thread(self.ser_event,self.queue,self.conf)
+                self.serial1Thread.setDaemon( True )
+                self.clcThread.setDaemon( True )
+                self.serial1Thread.start()
+                self.clcThread.start()
+            if self.modbus_cnt:
+                self.queue1.put("update")
+            self.modbus_cnt=True
 
             self.ser_clc_event.set()
             self.ser_event.set()
+            # self.ser_clc_event.set()
+            # self.ser_event.set()
             # self.serial2Thread = ser1Thread( )__init__(self,com,bard,func,fmt,regs,rege,devs,deve):
             # create a pubsub receiver
             pub.subscribe( self.ser_data_update, 'update' )
@@ -360,29 +503,45 @@ class MainWindow(wx.Frame):
 
             self.ser_clc_event.clear()
             self.ser_event.clear()
+            # self.stop_thread(self.serial1Thread)
+            # self.stop_thread( self.clcThread )
+            # self.serial1Thread.join()
+            # self.clcThread.join()
+
+
+            # print( fmt )
     def stop(self, event):
         event.Skip()
 
 
     def communication(self, event):
         event.Skip()
+    def dis_fmt_update(self):
+        #self.dis_fmt[地址]
+        #self.dis_fmt_name[对应名称]
+        title_str = self.conf.get( "excel", "disp_fmt" )
+        print( title_str )
+        str_title = re.findall( r"{.+}", title_str )
+        title = [i.replace( '{', '' ).replace( '}', '' ) for i in str_title]
+        print( title )
+        for i in title:
+            temp1 = i.split( ',' )
+            self.dis_fmt_name.append( temp1[0] )
+            t=[int(x) for x in temp1[1:]]
+            self.dis_fmt.append(t)
+        # print(self.dis_fmt)
+
+
 
     def switch(self, event):
         event.Skip()
-        title_str = self.conf.get("excel","disp_fmt")
-        print(title_str)
-        str_title = re.findall(r"{.+}",title_str )
-        title=[ i.replace('{','').replace('}','') for i in str_title ]
-        temp2=[]
-        print( title )
-        for i in title:
-            temp1=i.split(',')
-            temp2.append(temp1[0])
-        print(temp2)
-        self.m_staticText12.SetLabelText(temp2[self.swit])
-        self.swit += 1
-        if self.swit >= len(temp2):
-            self.swit=0
+        if self.conf.getboolean( "excel", "disp_set" ) is True:
+          self.dis_fmt_update()#数据采集
+          self.m_grid2.ClearGrid()
+          self.m_staticText12.SetLabelText(self.dis_fmt_name[self.swit])
+          self.swit += 1
+          if self.swit >= len(self.dis_fmt_name):
+              self.swit=0
 
 
 
